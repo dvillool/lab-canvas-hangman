f35742780f79ccd213053040833dcdde14817df3
ventdlnord <ventdlnord@gmail.com>
2017-10-26 21:27:04 +0200

    first contact

--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,26 +1,42 @@
-var hangman;
+'use strict';
 
-function Hangman() {
-
+function Hangman(secretWord, guessedLetters) {
+  this.words = ["Annakin", "Leia", "Luke", "Chewbacca", "HanSolo", "DeathStar", "Ewooks"];
+  this.secretWord = "";
+  this.letters = [];
+  this.guessedLetters = guessedLetters;
+  this.errorsLeft = 10;
 }
 
-Hangman.prototype._getWord = function () {
-
+Hangman.prototype._getWord = function() {
+  return (this.words[Math.floor(Math.random() * this.words.length)]);
 };
 
 Hangman.prototype._checkIfLetter = function(keyCode) {
-
+  if (65 <= keyCode && keyCode <= 90) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 Hangman.prototype._checkClickedLetters = function(key) {
 
+  for (var i = 0; i < this.letters.length; i++) {
+    if (key !== this.letters[i]) {
+      this._checkIfLetter (key);
+      return true;
+    } else {
+      return false;
+    }
+  }
 };
 
-Hangman.prototype._addCorrectLetter = function(i){
+Hangman.prototype._addCorrectLetter = function(i) {
 
 };
 
-Hangman.prototype._addWrongLetter = function (letter){
+Hangman.prototype._addWrongLetter = function(letter) {
 
 };
 
@@ -32,7 +48,7 @@ Hangman.prototype._checkWinner = function() {
 
 };
 
-document.getElementById("start-game-button").onclick = function(){
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
@@ -40,3 +56,5 @@ document.getElementById("start-game-button").onclick = function(){
 document.onkeydown = function(e) {
 
 };
+
+var testGame = new Hangman();
